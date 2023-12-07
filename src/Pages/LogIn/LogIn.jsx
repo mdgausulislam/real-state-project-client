c48e626feaea810986f80a0423a5194ac97e7797
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-07 18:34:02 +0600

    use react hook form sign and sign up

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -1,6 +1,9 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const LogIn = () => {
+  const { register, handleSubmit} = useForm();
+  const onSubmit = data => console.log(data);
   return (
     <div className="lg:mx-96 md:mx-48 max-h-screen bg-base-200">
       <div className="hero-content lg:px-16 md:px-12 flex-col">
@@ -8,18 +11,18 @@ const LogIn = () => {
           <h1 className="text-5xl font-bold">Log in!</h1>
         </div>
         <div className="card shrink-0 shadow-2xl bg-base-100 w-full">
-          <form className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="email" placeholder="email" className="input input-bordered" required />
+              <input type="email" placeholder="email" name="email" {...register("email")} className="input input-bordered" required />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="password" placeholder="password" className="input input-bordered" required />
+              <input type="password" placeholder="password" name="password" {...register("password", { maxLength: 6 })} className="input input-bordered" required />
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
               </label>
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,6 +1,10 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
     <div className="lg:mx-96 md:mx-48 max-h-screen bg-base-200">
       <div className="hero-content lg:px-16 md:px-12 flex-col">
@@ -8,27 +12,28 @@ const SignUp = () => {
           <h1 className="text-5xl font-bold">Sign Up!</h1>
         </div>
         <div className="card shrink-0 shadow-2xl bg-base-100 w-full">
-          <form className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">UserName</span>
               </label>
-              <input type="email" placeholder="Username..." className="input input-bordered" required />
+              <input type="text" placeholder="Username..." name="username" {...register("username")} className="input input-bordered" required />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="email" placeholder="email" className="input input-bordered" required />
+              <input type="email" placeholder="email" name="email" {...register("email")} className="input input-bordered" required />
+              {errors.mail && <p role="alert">{errors.mail?.message}</p>}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="password" placeholder="password" className="input input-bordered" required />
+              <input type="password" placeholder="password" name="password" {...register("password", { maxLength: 6 })} className="input input-bordered" required />
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Sign Up</button>
+              <input type="submit" value="Sign Up" className="btn btn-primary" />
             </div>
           </form>
           <div className="text-center mb-5">
