2c136b65d4b0f0168600cbe87cf89fb48f1face5
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-07 22:14:00 +0600

    sign up successfully create and using dorm data

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,9 +1,37 @@
-import { useForm } from "react-hook-form";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value, // Use e.target.name to capture input field names
+    });
+  };
+
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData), // Verify formData has all required fields
+    });
+
+    if (!res.ok) {
+      const errorResponse = await res.json(); // Parse error response
+      console.error("Error:", errorResponse); // Log the error response for debugging
+      // Handle error, show error message to the user, etc.
+    } else {
+      const data = await res.json();
+      console.log(data); // Log successful response
+      // Handle successful signup if needed
+    }
+  };
 
   return (
     <div className="lg:mx-96 md:mx-48 max-h-screen bg-base-200">
@@ -12,32 +40,64 @@ const SignUp = () => {
           <h1 className="text-5xl font-bold">Sign Up!</h1>
         </div>
         <div className="card shrink-0 shadow-2xl bg-base-100 w-full">
-          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+          <form onSubmit={handleSubmit} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">UserName</span>
               </label>
-              <input type="text" placeholder="Username..." name="username" {...register("username")} className="input input-bordered" required />
+              <input
+                type="text"
+                placeholder="UserName..."
+                name="userName"
+                onChange={handleChange}
+                className="input input-bordered"
+                required
+              />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="email" placeholder="email" name="email" {...register("email")} className="input input-bordered" required />
-              {errors.mail && <p role="alert">{errors.mail?.message}</p>}
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                onChange={handleChange}
+                className="input input-bordered"
+                required
+              />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="password" placeholder="password" name="password" {...register("password", { maxLength: 6 })} className="input input-bordered" required />
+              <input
+                type="password"
+                placeholder="password"
+                name="password" // Ensure the name attribute is set to "password"
+                onChange={handleChange}
+                className="input input-bordered"
+                required
+              />
             </div>
-            <div className="form-control mt-6">
-              <input type="submit" value="Sign Up" className="btn btn-primary" />
-            </div>
+            {/* <div className="form-control mt-6">
+              <input
+                type="submit"
+                value="Sign Up"
+                className="btn btn-primary"
+              />
+            </div> */}
+            <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+              Sign Up
+            </button>
           </form>
           <div className="text-center mb-5">
-            <p>Have an account?<span className="link link-hover text-blue-600"><Link to='/login'>Log in</Link></span></p>
+            <p>
+              Have an account?
+              <span className="link link-hover text-blue-600">
+                <Link to="/login">Log in</Link>
+              </span>
+            </p>
           </div>
         </div>
       </div>
