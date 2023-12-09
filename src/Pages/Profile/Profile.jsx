1dcfa891542c9b3a243043215df880e1cdb4fe36
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-09 17:53:45 +0600

    complete image upload successfully

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,8 +1,58 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../../firebase/firebase.config";
 
 const Profile = () => {
+    const fileRef = useRef(null);
     const { currentUser } = useSelector((state) => state.user);
+    const [file, setFile] = useState(undefined);
+    const [filePerc, setFilePerc] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(false);
+    const [formData, setFormData] = useState({});
+    // const [updateSuccess, setUpdateSuccess] = useState(false);
+    // const [showListingsError, setShowListingsError] = useState(false);
+    // const [userListings, setUserListings] = useState([]);
+    // const dispatch = useDispatch();
+    // console.log(filePerc);
+    // console.log(fileUploadError);
+    // console.log(formData);
+
+    // firebase storage
+    // allow read;
+    // allow write: if
+    // request.resource.size < 2 * 1024 * 1024 &&
+    // request.resource.contentType.matches('image/.*')
+
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
+
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setFilePerc(Math.round(progress));
+            },
+            (error) => {
+                setFileUploadError(true);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+                    setFormData({ ...formData, avatar: downloadURL })
+                );
+            }
+        );
+    };
     return (
         <div className="lg:mx-96 md:mx-48 max-h-screen bg-base-200">
             <div className="hero-content lg:px-16 md:px-12 flex-col">
@@ -12,8 +62,25 @@ const Profile = () => {
                 <div className="card shrink-0 shadow-2xl bg-base-100 w-full">
                     <form className="card-body">
                         <div className="form-control">
-
-                            <img src={currentUser.avatar} alt="profile" />
+                            <input
+                                onChange={(e) => setFile(e.target.files[0])} type="file"
+                                ref={fileRef}
+                                hidden
+                                accept="image/*" />
+                            <img onClick={() => fileRef.current.click()} className="cursor-pointer" src={formData.avatar || currentUser.avatar} alt="profile" />
+                            <p className='text-sm self-center'>
+                                {fileUploadError ? (
+                                    <span className='text-red-700'>
+                                        Error Image upload (image must be less than 2 mb)
+                                    </span>
+                                ) : filePerc > 0 && filePerc < 100 ? (
+                                    <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+                                ) : filePerc === 100 ? (
+                                    <span className='text-green-700'>Image successfully uploaded!</span>
+                                ) : (
+                                    ''
+                                )}
+                            </p>
                         </div>
                         <div className="form-control">
                             <label className="label">
