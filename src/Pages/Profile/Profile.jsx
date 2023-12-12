146d5adea1b318b38e1735a6368bbbdaa5fda7f8
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-12 22:02:50 +0600

    crteate a user listing and user delete handle

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/
 import { app } from "../../firebase/firebase.config";
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from "../../redux/user/userSlice";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const Profile = () => {
@@ -15,6 +15,8 @@ const Profile = () => {
     const [fileUploadError, setFileUploadError] = useState(false);
     const [formData, setFormData] = useState({});
     const [updateSuccess, setUpdateSuccess] = useState(false);
+    const [showListingsError, setShowListingsError] = useState(false);
+    const [userListings, setUserListings] = useState([]);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
@@ -101,6 +103,39 @@ const Profile = () => {
         }
     };
 
+    const handleShowListings = async () => {
+        try {
+            setShowListingsError(false);
+            const res = await axios.get(`/api/user/listings/${currentUser._id}`);
+            if (res.data.success === false) {
+                setShowListingsError(true);
+                return;
+            }
+            setUserListings(res.data);
+        } catch (error) {
+            setShowListingsError(true);
+        }
+    };
+
+    const handleListingDelete = async (listingId) => {
+        try {
+            const res = await fetch(`/api/listing/delete/${listingId}`, {
+                method: 'DELETE',
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                console.log(data.message);
+                return;
+            }
+
+            setUserListings((prev) =>
+                prev.filter((listing) => listing._id !== listingId)
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     return (
         <div className="lg:mx-96 md:mx-48 max-h-screen bg-base-200">
             <div className="hero-content lg:px-16 md:px-12 flex-col">
@@ -182,12 +217,18 @@ const Profile = () => {
                         >
                             {loading ? 'loading' : 'Update'}
                         </button>
-                        <button
+                        {/* <button
 
                             className="bg-blue-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
                         >
                             Create Listing
-                        </button>
+                        </button> */}
+                        <Link
+                            className='bg-blue-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+                            to={'/create-listing'}
+                        >
+                            Create Listing
+                        </Link>
                     </form>
                     <p className="text-red-700 self-center text-sm">{error ? error : ''}</p>
                     <p className='text-green-700 mt-5'>
@@ -198,6 +239,52 @@ const Profile = () => {
                         <button onClick={handleSignOut} className="text-green-700 font-bold">Sign Out</button>
                     </div>
 
+                    <button onClick={handleShowListings} className='text-green-700 w-full'>
+                        Show Listings
+                    </button>
+                    <p className='text-red-700 mt-5'>
+                        {showListingsError ? 'Error showing listings' : ''}
+                    </p>
+
+                    {userListings && userListings.length > 0 && (
+                        <div className='flex flex-col gap-4'>
+                            <h1 className='text-center mt-7 text-2xl font-semibold'>
+                                Your Listings
+                            </h1>
+                            {userListings.map((listing) => (
+                                <div
+                                    key={listing._id}
+                                    className='border rounded-lg p-3 flex justify-between items-center gap-4'
+                                >
+                                    <Link to={`/listing/${listing._id}`}>
+                                        <img
+                                            src={listing.imageUrls[0]}
+                                            alt='listing cover'
+                                            className='h-16 w-16 object-contain'
+                                        />
+                                    </Link>
+                                    <Link
+                                        className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                                        to={`/listing/${listing._id}`}
+                                    >
+                                        <p>{listing.name}</p>
+                                    </Link>
+
+                                    <div className='flex flex-col item-center'>
+                                        <button
+                                            onClick={() => handleListingDelete(listing._id)}
+                                            className='text-red-700 uppercase'
+                                        >
+                                            Delete
+                                        </button>
+                                        <Link to={`/update-listing/${listing._id}`}>
+                                            <button className='text-green-700 uppercase'>Edit</button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
